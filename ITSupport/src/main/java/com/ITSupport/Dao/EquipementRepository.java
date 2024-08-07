e366d6563be17088e5cdf2dbf83189f2a3eab89b
basmaqtb <qotbibasma8@gmail.com>
2024-08-07 17:58:44 +0100

    "repository d'equipement"

---
+++ ITSupport/src/main/java/com/ITSupport/Dao/EquipementRepository.java
@@ -0,0 +1,7 @@
+package com.ITSupport.Dao;
+
+import com.ITSupport.Models.Equipement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EquipementRepository extends JpaRepository<Equipement, Long> {
+}
